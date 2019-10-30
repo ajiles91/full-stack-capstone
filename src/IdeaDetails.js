bf106e74158942d4401ac6a30969f6372164ea1d
Amber <ajiles91@gmail.com>
2019-10-29 20:32:36 -0400

    update

--- src/IdeaDetails.js
+++ src/IdeaDetails.js
@@ -23,33 +23,35 @@ const IdeaDetails = (props) => {
     const data = ideasCtx.ideas.find((idea) => {
         return idea.id === parseInt(id)
     })
-    console.log(data)
+    console.log(data.claimed)
     const [claimed,setClaimed] = useState(data.claimed)
   
-    const handleClaimedClick = () => {
+    // const handleClaimedClick = () => {
         // setClaimed(true)
-
-        //PATCH to the resource id = 1
-        // update that task is completed
-        // fetch(`${config.API_BASE_URL}/idea/${props.id}`, {
-        //     method: ‘PATCH’,
-        //     body: JSON.stringify({
-        //     completed: true
-        //     }),
-        //     headers: {
-        //     “Content-type”: “application/json; charset=UTF-8”
+         //PATCH to the resource id = 1
+            // update that task is completed
+            // fetch(`${config.API_BASE_URL}/idea/${data.id}`, {
+            //     method: ‘PATCH’,
+            //     body: JSON.stringify({
+            //     claimed: false
+            //     }),
+            //     headers: {
+            //     “Content-type”: “application/json; charset=UTF-8”
         //     }
         //     })
         //     .then(response => response.json())
         //     .then(json => console.log(json))
-    }
+       
+    // }
+    // 
+       
 
-    const handleReleasedClick = () => {
+    // const handleReleasedClick = () => {
         // setClaimed(false)
 
         //PATCH to the resource id = 1
         // update that task is completed
-        // fetch(`${config.API_BASE_URL}/idea/${props.id}`, {
+        // fetch(`${config.API_BASE_URL}/idea/${data.id}`, {
         //     method: ‘PATCH’,
         //     body: JSON.stringify({
         //     claimed: false
@@ -61,8 +63,8 @@ const IdeaDetails = (props) => {
         //     .then(response => response.json())
         //     .then(json => console.log(json))
        
-    }
-    const index = id - 1;
+    // }
+    // 
     const claimedButton = claimed ? null : <button onClick={handleClaimedClick}> Claim Idea</button>
     const releasedButton = (claimed === false )? null : <button onClick={handleReleasedClick}> Release Idea</button>
 
@@ -70,30 +72,24 @@ const IdeaDetails = (props) => {
    
     return (
         <>
-            {/* <p>Id: {data.id}</p> */}
-            
-            
-            {/* <p>Name: {data.ideaname}</p>
+            <p>Id: {data.id}</p> */}
+            <p>Name: {data.ideaname}</p>
             <p>Description: {data.ideasummary}</p>
             <p>Status: {claimed ? 'claimed': 'unclaimed'}</p>
             <a href={`mailto:${data.email}`} rel="noopener noreferrer" target="_blank" className='button-border'>
                 <span className='button-inner'>Contact Author</span>
-            </a> */}
-
-            <p>Id: {ideasCtx.ideasTest[id] && ideasCtx.ideasTest[id].id}</p>
-            <p>Description: {ideasCtx.ideasTest[id] && ideasCtx.ideasTest[id].ideasummary}</p>
-            <p>Status: {ideasCtx.ideasTest[id] && ideasCtx.ideasTest[id].claimed} ? 'claimed': 'unclaimed'}</p>
-            <a href={`mailto:${data.email}`} rel="noopener noreferrer" target="_blank" className='button-border'>
-                <span className='button-inner'>Contact Author</span>
             </a>
 
-           {claimedButton}
-           {releasedButton}
-           <Link to='/'>
+           
+
+
+           {/* {claimedButton}
+           {releasedButton} */}
+        <Link to='/'>
                 <button>
-                    to idea list
+                     to idea list
                 </button>
-           </Link>
+            </Link>
         </>
 
     )
