94feb4119293195a51b3b17d3f677d3bb612e676
Amber <ajiles91@gmail.com>
2019-11-29 22:10:36 -0500

    trying to clear smoke test

--- src/UserPage/UserPage.js
+++ src/UserPage/UserPage.js
@@ -4,6 +4,30 @@ import FormContext from "../FormContext";
 import './UserPage.css'
 
 const UserPage = () => {
+  UserPage.defaultProps = {
+    idea: [
+      {
+        id: 1, 
+        ideaName:'build an ecommerce website', 
+        ideaSummary:'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium',
+        authorName:'Name 1',
+        email:'name1@email.com',
+        claimed: true,
+        submitted: false
+      },
+    
+      {
+        id: 2,
+        ideaName: 'develop a singles matching app',
+        ideaSummary: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium',
+        authorName: 'Name 2',
+        email: 'name2@email.com',
+        claimed:false,
+        submitted: false
+    },
+    ]
+
+  }
 
  const ideasCtx = React.useContext(FormContext);
   
