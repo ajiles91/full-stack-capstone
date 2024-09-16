513b54dca15278dd63fdd2c35226f618b0df0e59
ajiles91 <ajiles91@gmail.com>
2024-09-16 02:04:46 -0500

    switch component depreciated becomes routes componet

--- src/App.js
+++ src/App.js
@@ -5,57 +5,70 @@ import CreateIdea from "./CreateIdea/CreateIdea";
 import IdeaDetails from "./IdeaDetails/IdeaDetails";
 
 import FormContext from "./FormContext";
-import config from './config'
+import config from "./config";
 
 export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       ideas: [],
-    }
+    };
   }
 
   componentDidMount() {
     fetch(`${config.API_BASE_URL}`)
-      .then(response => response.json())
-      .then(json => {
+      .then((response) => response.json())
+      .then((json) => {
         this.setState({
-          ideas: json
-        })
-      })
+          ideas: json,
+        });
+      });
   }
 
   updateClaimedOnMainPage = () => {
     fetch(`${config.API_BASE_URL}`)
-      .then(response => response.json())
-      .then(json => {
+      .then((response) => response.json())
+      .then((json) => {
         this.setState({
-          ideas: json
-        })
-      })
-  }
+          ideas: json,
+        });
+      });
+  };
 
   render() {
     return (
       <div>
         <FormContext.Provider value={{ ideas: this.state.ideas }}>
           <BrowserRouter>
-            <Switch>
-              <Route path="/" exact
-                render={props => (
-                  <UserPage updateClaimedOnMainPage={this.updateClaimedOnMainPage} {...props} />
-                )} />
+            <Routes>
+              <Route
+                path="/"
+                exact
+                render={(props) => (
+                  <UserPage
+                    updateClaimedOnMainPage={this.updateClaimedOnMainPage}
+                    {...props}
+                  />
+                )}
+              />
               <Route
                 path="/idea/:id"
-                render={props => <IdeaDetails updateClaimedOnMainPage={this.updateClaimedOnMainPage} {...props} />}
+                render={(props) => (
+                  <IdeaDetails
+                    updateClaimedOnMainPage={this.updateClaimedOnMainPage}
+                    {...props}
+                  />
+                )}
               />
               <Route
                 path="/idea"
-                render={props => (
-                  <CreateIdea updateClaimedOnMainPage={this.updateClaimedOnMainPage} />
+                render={(props) => (
+                  <CreateIdea
+                    updateClaimedOnMainPage={this.updateClaimedOnMainPage}
+                  />
                 )}
               />
-            </Switch>
+            </Routes>
           </BrowserRouter>
         </FormContext.Provider>
       </div>
