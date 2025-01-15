261b53bba569f45a6a403f1d5959792172cfdb99
ajiles91 <ajiles91@gmail.com>
2025-01-15 00:26:58 -0600

    update render for react19

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { createRoot } from "react-dom/client";
 
-ReactDOM.render(<App className="full-page" />, document.getElementById('root')
-);
+const root = createRoot(document.getElementById("root"));
+
+root.render(<App className="full-page" />);
